0076f2f529a58a065e61bf9e820020bbfcab0e8f
Krzysztof Sowul <krzysiek.sowul@gmail.com>
2019-11-25 23:58:31 +0100

    Lab1 done

--- Blok 1/Odczyt próbek/Odczyt_Probek/Odczyt_Probek/MainWindow.xaml.cs
+++ Blok 1/Odczyt próbek/Odczyt_Probek/Odczyt_Probek/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using LoadFileMethods;
+using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +23,103 @@ namespace Odczyt_Probek
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string _descriptionFile = string.Empty;
+        private string _valuesFile = string.Empty;
+
+        private List<List<string>> values;
+        private List<bool> areSymbols;
+        private List<string> names;
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void AddDescriptionButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                _descriptionFile = openFileDialog.FileName;
+            }
+
+            CheckIfFilesChosen();
+        }
+
+        private void AddValuesButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                _valuesFile = openFileDialog.FileName;
+            }
+
+            CheckIfFilesChosen();
+        }
+
+        private void CheckIfFilesChosen()
+        {
+            if (!string.IsNullOrWhiteSpace(_descriptionFile) && !string.IsNullOrWhiteSpace(_valuesFile))
+            {
+                LoadDataButton.IsEnabled = true;
+            }
+        }
+
+        private void LoadDataButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                FileExtensionMethods.wczytaj_baze_probek_z_tekstem(_valuesFile, _descriptionFile, out values, out areSymbols, out names);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+
+                CleanProgram();
+
+                return;
+            }
+
+            values.ForEach(row =>
+            {
+                for (int i = 0; i < row.Count; i++)
+                {
+                    if (areSymbols[i])
+                    {
+                        row[i] = $"\"{row[i]}\"";
+                    }
+                }
+            });
+
+            DataTable data = new DataTable();
+
+            foreach (var name in names)
+            {
+                data.Columns.Add(name);
+            }
+
+            foreach (var row in values)
+            {
+                DataRow dataRow = data.NewRow();
+                for (int i=0; i<names.Count;i++)
+                {
+                    dataRow[i] = row[i];
+                }
+                data.Rows.Add(dataRow);
+            }
+
+
+            LoadedDataGrid.ItemsSource = data.DefaultView;
+
+            CleanProgram();
+        }
+
+        private void CleanProgram()
+        {
+            _descriptionFile = string.Empty;
+            _valuesFile = string.Empty;
+            LoadDataButton.IsEnabled = false;
+        }
+
     }
 }
