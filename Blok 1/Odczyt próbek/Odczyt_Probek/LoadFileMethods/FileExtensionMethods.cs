be1a62f9794c7cbdbfb3024f267e2fb0c20d545f
Krzysztof Sowul <krzysztof.sowul@billennium.pl>
2019-11-26 08:59:05 +0100

    Adjusted wczytaj_baze_probek_z_tekstem to other txt files

--- Blok 1/Odczyt próbek/Odczyt_Probek/LoadFileMethods/FileExtensionMethods.cs
+++ Blok 1/Odczyt próbek/Odczyt_Probek/LoadFileMethods/FileExtensionMethods.cs
@@ -7,10 +7,10 @@ namespace LoadFileMethods
 {
     public static class FileExtensionMethods
     {
-        public static void wczytaj_baze_probek_z_tekstem(string nazwa_pliku_z_wartosciami, 
+        public static void wczytaj_baze_probek_z_tekstem(string nazwa_pliku_z_wartosciami,
                                                          string nazwa_pliku_z_opisem_atr,
-                                                         out List<List<string>> probki, 
-                                                         out List<bool> czy_atr_symb, 
+                                                         out List<List<string>> probki,
+                                                         out List<bool> czy_atr_symb,
                                                          out List<string> nazwy_atr)
         {
             probki = new List<List<string>>();
@@ -19,11 +19,17 @@ namespace LoadFileMethods
 
             var descriptions = File.ReadAllText(nazwa_pliku_z_opisem_atr);
 
+            descriptions = RefactorToSpaceSeparator(descriptions);
+
             var descriptionRows = descriptions.Split('\n');
+
+            descriptionRows = RemoveAllEmptyOccurances(descriptionRows);
 
             foreach (var descriptionRow in descriptionRows)
             {
                 var descriptionRowValues = descriptionRow.Split(' ');
+
+                descriptionRowValues = RemoveAllEmptyOccurances(descriptionRowValues);
 
                 if (descriptionRowValues.Length != 2)
                 {
@@ -43,26 +49,74 @@ namespace LoadFileMethods
 
             var data = File.ReadAllText(nazwa_pliku_z_wartosciami);
 
-            data = data.Replace("\r", "");
+            data = RefactorToSpaceSeparator(data);
 
             var dataRows = data.Split('\n');
 
-            if (dataRows.Any(r => r.Split(' ').Length != dataRows[0].Split(' ').Length))
-            {
-                throw new Exception("Plik z wartościami jest błędny. Wiersze nie mają takich samych długości");
-            }
+            dataRows = RemoveAllEmptyOccurances(dataRows);
+
+            int rowLength = 0;
 
             foreach (var valueRow in dataRows)
             {
-                var valueRowValues = valueRow.Split(' ').ToList();
+                var valueRowValues = valueRow.Split(' ');
 
-                probki.Add(valueRowValues);
+                valueRowValues = RemoveAllEmptyOccurances(valueRowValues);
+
+                if (rowLength != 0)
+                {
+                    if (valueRowValues.Length != rowLength)
+                    {
+                        throw new Exception("Plik z wartościami jest błędny. Wiersze nie mają takich samych długości");
+                    }
+                }
+
+                rowLength = valueRowValues.Length;
+
+                probki.Add(valueRowValues.ToList());
             }
 
             if (probki[0].Count() != numberOfColumns)
             {
                 throw new Exception("Niepoprawna para plików opisu i wartości. Liczba kolumn się różni.");
             }
+        }
+
+        private static string RefactorToSpaceSeparator(string text)
+        {
+            text = text.Replace('\r', ' ');
+            text = text.Replace('\t', ' ');
+
+            string oldText = text;
+            string newText;
+
+            while (true)
+            {
+                newText = oldText.Replace("  ", " ");
+
+                if (newText == oldText)
+                {
+                    return oldText;
+                }
+
+                oldText = newText;
+            }
+        }
+
+        private static string[] RemoveAllEmptyOccurances(string[] texts)
+        {
+            var list = texts.ToList();
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (string.IsNullOrWhiteSpace(list[i]))
+                {
+                    list.Remove(list[i]);
+                    i--;
+                }
+            }
+
+            return list.ToArray();
         }
     }
 }
